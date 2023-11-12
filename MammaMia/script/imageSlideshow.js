2bece1782814cd29f296c6f2c81e5ad11b4a2e69
johanneskhage <johannesk.hage@gmail.com>
2023-11-12 14:40:11 +0100

    Forbedret slideshow

--- MammaMia/script/imageSlideshow.js
+++ MammaMia/script/imageSlideshow.js
@@ -3,7 +3,6 @@ const slides = document.querySelectorAll('.slide');
 
 const indicators = document.querySelectorAll('.indicator');
 
-
 function showSlide(n) {
   if (n < 0) {
     slideIndex = slides.length - 1;
@@ -13,13 +12,20 @@ function showSlide(n) {
     slideIndex = n;
   }
 
+  /*
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none';
     indicators[i].style.opacity = 0.5;
   }
 
+
   slides[slideIndex].style.display = 'block';
   indicators[slideIndex].style.opacity = 1;
+  */
+
+  posLeft(transformIndex(slideIndex - 1));
+  posRight(transformIndex(slideIndex + 1));
+  posMiddle(slideIndex);
 }
 
 function plusSlides(n) {
@@ -34,7 +40,42 @@ setInterval(function () {
 //shows the first slide
 showSlide(slideIndex);
 
+
 //pressing the indicators will swap to that picture
 function currentSlide(n){
   showSlide(n)
+}
+
+function posMiddle(slide){
+  slides[slide].style.transform = 'scale(1)';
+  slides[slide].style.marginLeft = '20%';
+  slides[slide].style.display = 'inline-block';
+  slides[slide].style.position = 'absolute';
+  slides[slide].style.zIndex = '2';
+}
+
+function posLeft(slide){
+  slides[slide].style.transform = 'scale(0.75)';
+  slides[slide].style.marginLeft = '0%';
+  slides[slide].style.display = 'inline-block';
+  slides[slide].style.position = 'absolute';
+  slides[slide].style.zIndex = '1';
+}
+
+function posRight(slide){
+  slides[slide].style.transform = 'scale(0.75)';
+  slides[slide].style.marginLeft = '40%';
+  slides[slide].style.display = 'inline-block';
+  slides[slide].style.position = 'absolute';
+  slides[slide].style.zIndex = '1';
+}
+
+function transformIndex(n){
+  if (n < 0) {
+    return slides.length - 1;
+  } else if (n >= slides.length) {
+    return 0;
+  } else {
+    return n;
+  }
 }
