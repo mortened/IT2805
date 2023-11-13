ae7b09e8b06f4bfc6fa017ba01b66f4ea1e5389c
etfroeland <etfroeland@gmail.com>
2023-11-13 10:57:39 +0100

    Added comments

--- MammaMia/script/subsriptionPopup.js
+++ MammaMia/script/subsriptionPopup.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     const hasSeenPopup = document.cookie.includes("popupShown=true");
+
+    //Checks if the user has seen the popup before
     if (!hasSeenPopup) {
         document.getElementById("popup").style.display = "block";
     }
 
-    
+    //Removes the popup is the user closes the popup and adds a cookie
     document.getElementById("close-popup").addEventListener("click", function() {
         document.getElementById("popup").style.display = "none";
         document.cookie = "popupShown=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
@@ -13,6 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const subscriptionForm = document.getElementById("subscription-form");
 
+    //Submits the email, currently nothing is done with the user email
     subscriptionForm.addEventListener("submit", function(event) {
         event.preventDefault(); // Prevent the default form submission
 
