005156633b78b7fdff75ff740cf65347e23cc0fc
Morten Sandbæk Edvardsen <mortensandbaekedvardsen@dhcp-10-22-19-36.wlan.ntnu.no>
2023-11-13 11:13:58 +0100

    Comments menu

--- MammaMia/script/allergenInfo.js
+++ MammaMia/script/allergenInfo.js
@@ -1,3 +1,4 @@
+//Menu items and their respective allergens if they have one
 const allergyDict = {
     bruschetta: ["Gluten"],
     caprese_salad: ["Dairy"],
@@ -25,37 +26,44 @@ const allergyDict = {
     amaretto_disaronno: ["Nuts"]
 };
 
-
+//Access all menu items with class "allergy"
 const allergyTags = document.getElementsByClassName("allergy");
+//Declare the popup div
 let allergyDiv;
 
+//Add event listener to each menu item
 for (let i = 0; i < allergyTags.length; i++) {
     const element = allergyTags[i];
-    console.log(element);
     element.addEventListener("click", function(event) {
+        //Prevent the click event from bubbling up to the document
         event.stopPropagation();
+        //Hide any existing allergy info
         hideAllergyInfo();
+        //If the menu item has an allergy, display the allergy info
         if (allergyDict.hasOwnProperty(element.id)) {
-            console.log(allergyDict[element.id]);
+            //Get the position of the menu item
             const elementRect = element.getBoundingClientRect();
             const X = elementRect.left + window.scrollX;
             const Y = elementRect.top + window.scrollY;
-            console.log(X,Y);
+            //Display the allergy info
             displayAllergyInfo(element.id, X, Y)
         }
     });
 }
 
+//Hide the allergy info if the user clicks anywhere else on the document other than the popup div
 document.addEventListener("click", function(){
     hideAllergyInfo();
 })
 
-
+//Display the allergy info for the menu item at the given position
 function displayAllergyInfo(menuItem, X, Y) {
 
+   
     if(allergyDict.hasOwnProperty(menuItem)){
-
+        //Create the popup div
         allergyDiv = document.createElement("div");
+        //Set the style of the popup div
         allergyDiv.style.position = "absolute";
         allergyDiv.style.left = X-180 + "px";
         allergyDiv.style.top = Y + "px";
@@ -66,11 +74,13 @@ function displayAllergyInfo(menuItem, X, Y) {
         allergyDiv.style.color = "#FFFFFF";
         allergyDiv.style.borderRadius = "10px";
 
+        //Create the allergy info
         const allergyHeading = document.createElement("p");
         allergyHeading.style.fontSize = "35px";
         allergyHeading.textContent = "Allergies";
         allergyDiv.appendChild(allergyHeading);
 
+        //Create the close button
         const closeX = document.createElement("h1");
         closeX.textContent = "x";
         closeX.style.margin = "0";
@@ -83,32 +93,36 @@ function displayAllergyInfo(menuItem, X, Y) {
         closeX.style.textAlign = "right";
         closeX.style.marginTop = "-10px";
         
+        //Add the close button to the popup div
         allergyDiv.appendChild(closeX);
+        //Add event listener to the close button that closes the div
         closeX.addEventListener("click", function(event) {
             event.stopPropagation();
             hideAllergyInfo();
         });
 
-
+        //Create the list of allergies for the menu item
         const allergies = allergyDict[menuItem];
         const allergyList = document.createElement("ul");
         allergyList.style.margin = "10px 0 10px 0";
         allergyDiv.appendChild(allergyList);
+        //Add each allergy to the list
         allergies.forEach(allergy => {
             const listItem = document.createElement("li");
             listItem.style.fontSize = "30px";
             listItem.textContent = allergy;
             allergyList.appendChild(listItem);
         });
-
+        //Add the popup div to the document
         document.body.appendChild(allergyDiv);
-
+        //Prevent the click event from bubbling up to the document
         allergyDiv.addEventListener("click", function(event) {
             event.stopPropagation();
         });
     }
 }
 
+//Hide the allergy info
 function hideAllergyInfo(){
     if(allergyDiv){
         allergyDiv.remove();
